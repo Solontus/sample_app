f80f042ad102a528412cb02caa58e508a35a630d
Vincent Chau <ws3chau@uwaterloo.ca>
2014-01-20 00:52:33 -0500

    Minor modifications to user system

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,7 @@ describe User do
 
   	describe "when email format is invalid" do
   		it "should be invalid" do
-  			addresses = %w[user@foo,com user_at_foo.org example.user@foo foo@bar_baz.com foo@bar+baz.com]
+  			addresses = %w[user@foo,com user_at_foo.org example.user@foo foo@bar_baz.com foo@bar+baz.com foobar@example..com]
   			addresses.each do |invalid_address|
   				@user.email = invalid_address
   				expect(@user).not_to be_valid
@@ -93,5 +93,15 @@ describe User do
         it { should_not eq user_for_invalid_password }
         specify { expect(user_for_invalid_password).to be_false }
       end 
+    end
+
+    describe "email address with mixed case" do
+      let(:mixed_case_email) { "Foo@ExAMPle.CoM"}
+
+      it "should be save as all lower-case" do
+        @user.email = mixed_case_email
+        @user.save
+        expect(@user.reload.email).to eq mixed_case_email.downcase
+      end
     end
 end
